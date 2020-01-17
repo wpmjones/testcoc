15aaa70ee286e4ff32e501b0f03c461432d1b38f
W. Patrick Jones <wpmjones@gmail.com>
2020-01-17 15:21:31 -0500

    add dates

--- README.md
+++ README.md
@@ -2,15 +2,50 @@
 RESTful API to test COC libraries and scripts
 
 # What it does
-Generate both static and random data which is both correct and incorrect so that you 
+Generate both static and random data which is either correct or incorrect so that you 
 can test your code for how it handles user input and API output.
 
 # Endpoints
-Correct clan & player tags  
-Incorrect clan & player tags  
-Supercell style timestamps
+###Player Tags  
+`/player_tag?limit=25&wrong_prefix=True`  
+Returns valid, properly structured player tags. 
+ 
+**Optional arguments:**  
+limit (integer): limit the list to a specific number of tags
+wrong_prefix (boolean): replace the # with some other character
+
+`/player_tag/garbled?limit=5&wrong_prefix=True`  
+Returns valid, improperly structured player tags representing possible input from end users. 
+This might include spaces where they don't belong, o's instead of 0's, mix of upper and 
+lowercase letters, and incorrect prefixes.
+
+All responses to this endpoint can be converted into valid player tags.
+  
+**Optional arguments:**  
+limit (integer): limit the list to a specific number of tags
+wrong_prefix (boolean): replace the # with some other character
+
+`/player_tag/invalid?limit=10`  
+Returns invalid, properly structed player tags. This might include tags with incorrect letters, 
+tags that are too long or too short, etc.  This is so that you can see what your code will do 
+with invalid tags. Wrong prefix is not an option here since we're testing invalid tags, not prefixes.
+  
+**Optional arguments:**  
+limit (integer): limit the list to a specific number of tags
+
+### Dates
+`/dates?date_type=past&limit=25`  
+Returns valid, Supercell style timestamps.
+  
+**Required argument:**  
+date_type (string): `past` or `future`
+
+**Optional argument:**  
+limit (integer): limit the list to a specific number of dates (default=255)
+
+
+
+### Coming soon
+Clan tags
 Valid & invalid player names names in a variety of languages
 Valid & invalid clan names in a variety of languages
- 
- # Usage
- TBD
--- cocrest.py
+++ cocrest.py
@@ -1,26 +1,155 @@
-from flask import Flask, request
-from flask_restful import Resource, Api
+import constants
+import random
+
+from datetime import datetime, timedelta
+from flask import Flask
+from flask_restful import Resource, Api, reqparse
+
 
 app = Flask(__name__)
 api = Api(app)
 
 
-class HelloWorld(Resource):
+class About(Resource):
     def get(self):
-        return {"about": "Hello world!"}
-
-    def post(self):
-        some_json = request.get_json()
-        return {"you sent": some_json}, 201
-
-
-class Multi(Resource):
-    def get(self, num):
-        return {"result": num * 10}
+        return {"about": "This API has been put together for the simple purpose of providing test data (both "
+                         "valid and invalid) so that developers can test their code without having to abuse the "
+                         "official COC API.  This is also a great tool to see how your code handles invalid input "
+                         "from end users.  This API is maintained by TubaKid.  If you have questions or concerns, "
+                         "you can find me on Discord at https://discord.gg/Eaja7gJ"}
 
 
-api.add_resource(HelloWorld, '/')
-api.add_resource(Multi, "/multi/<int:num>")
+class ValidPlayerTag(Resource):
+    def __init__(self):
+        self.reqparse = reqparse.RequestParser()
+        self.reqparse.add_argument("limit",
+                                   type=int,
+                                   required=False,
+                                   default=None,
+                                   help="Optional limit on number of tags returned.")
+        self.reqparse.add_argument("wrong_prefix",
+                                   type=bool,
+                                   required=False,
+                                   default=False,
+                                   help="Choose whether or not to responde with incorrect prefixes.")
+
+    def get(self):
+        args = self.reqparse.parse_args()
+        limit = args['limit']
+        wrong_prefix = args['wrong_prefix']
+        return_list = []
+        if limit:
+            while len(return_list) < limit:
+                x = random.choice(constants.valid_player_tags)
+                if x not in return_list:
+                    return_list.append(x)
+        else:
+            return_list = constants.valid_player_tags
+        if wrong_prefix:
+            return_list = [tag.replace("#", random.choice(constants.prefixes)) for tag in return_list]
+        return {"tags": return_list}
+
+
+class GarbledPlayerTag(Resource):
+    def __init__(self):
+        self.reqparse = reqparse.RequestParser()
+        self.reqparse.add_argument("limit",
+                                   type=int,
+                                   required=False,
+                                   default=None,
+                                   help="Optional limit on number of tags returned.")
+        self.reqparse.add_argument("wrong_prefix",
+                                   type=bool,
+                                   required=False,
+                                   default=False,
+                                   help="Choose whether or not to responde with incorrect prefixes.")
+
+    def get(self):
+        args = self.reqparse.parse_args()
+        limit = args['limit']
+        wrong_prefix = args['wrong_prefix']
+        return_list = []
+        if limit:
+            while len(return_list) < limit:
+                x = random.choice(constants.garbled_player_tags)
+                if x not in return_list:
+                    return_list.append(x)
+        else:
+            return_list = constants.garbled_player_tags
+        if wrong_prefix:
+            return_list = [tag.replace("#", random.choice(constants.prefixes)) for tag in return_list]
+        return {"tags": return_list}
+
+
+class InvalidPlayerTag(Resource):
+    def __init__(self):
+        self.reqparse = reqparse.RequestParser()
+        self.reqparse.add_argument("limit",
+                                   type=int,
+                                   required=False,
+                                   default=None,
+                                   help="Optional limit on number of tags returned.")
+        # self.reqparse.add_argument("wrong_prefix",
+        #                            type=bool,
+        #                            required=False,
+        #                            default=False,
+        #                            help="Choose whether or not to responde with incorrect prefixes.")
+
+    def get(self):
+        args = self.reqparse.parse_args()
+        limit = args['limit']
+        # wrong_prefix = args['wrong_prefix']
+        return_list = []
+        if limit:
+            while len(return_list) < limit:
+                if len(return_list) == len(constants.invalid_tags):
+                    break
+                x = random.choice(constants.invalid_tags)
+                if x not in return_list:
+                    return_list.append(x)
+        else:
+            return_list = constants.invalid_tags
+        return {"tags": return_list}
+
+
+class Dates(Resource):
+    def __init__(self):
+        self.reqparse = reqparse.RequestParser()
+        self.reqparse.add_argument("limit",
+                                   type=int,
+                                   required=False,
+                                   default=None,
+                                   help="Optional limit on number of dates returned.")
+        self.reqparse.add_argument("date_type",
+                                   type=str,
+                                   required=True,
+                                   help="Required: Past or Future")
+
+    def get(self):
+        args = self.reqparse.parse_args()
+        limit = args['limit']
+        tense = args['date_type'].lower()
+        return_list = []
+        if not limit:
+            limit = 255
+        while len(return_list) < limit:
+            if tense == "past":
+                new_date = datetime.utcnow() - timedelta(hours=random.randint(0, 168),
+                                                         minutes=random.randint(1, 59))
+            elif tense == "future":
+                new_date = datetime.utcnow() + timedelta(hours=random.randint(0, 168),
+                                                         minutes=random.randint(1, 59))
+            else:
+                return {"error": "You must provide a valid argument for date_type (either past or future)."}
+            return_list.append(new_date.strftime("%Y%m%dT%H%M%S.000Z"))
+        return {f"{tense}_dates": return_list}
+
+
+api.add_resource(About, '/')
+api.add_resource(ValidPlayerTag, "/player_tag")
+api.add_resource(GarbledPlayerTag, "/player_tag/garbled")
+api.add_resource(InvalidPlayerTag, "/player_tag/invalid")
+api.add_resource(Dates, "/dates")
 
 if __name__ == "__main__":
     app.run(debug=True)
